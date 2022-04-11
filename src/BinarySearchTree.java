c291d1bec574b2d6975c17f67a838599c25d1b57
BuildTools <unconfigured@null.spigotmc.org>
2022-04-11 15:02:51 -0400

    create delete method

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,5 +1,5 @@
 public class BinarySearchTree {
-    Node root;
+    protected Node root;
 
     public BinarySearchTree() {
         this.root = null;
@@ -15,12 +15,50 @@ public class BinarySearchTree {
             return new Node(data);
         }
 
-        if (data < current.data) {
+        if (data.compareTo(current.data) == -1) {
             current.left = this.insertNode(current.left, data);
-        } else if (data > current.data) {
+        } else if (data.compareTo(current.data) == 1) {
             current.right = this.insertNode(current.right, data);
         }
 
         return null;
     }
+
+    public Node delete(Integer data){
+        return deleteNode(this.root, data);
+    }
+
+    public Node deleteNode(Node current, Integer data){
+        if (root == null){
+            return root;
+        }
+
+        if(data < current.data){
+            current.left = this.insertNode(current.left, data);
+        }
+
+        else{
+            if (root.left == null){
+                return root.right;
+            }
+            else if (root.right == null){
+                return root.left;
+            }
+
+            //node with tw children
+            root.data = minValue(root.right);
+            root.right = deleteNode(root.right, root.data);
+        }
+        return root;
+    }
+
+    public int minValue(Node root){
+        int minimum = root.data;
+        while (root.left != null){
+            minimum = root.left.data;
+            root = root.left;
+        }
+        return minimum;
+    }
+
 }
